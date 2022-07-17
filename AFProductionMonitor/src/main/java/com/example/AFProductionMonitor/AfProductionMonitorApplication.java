eb925733e1404b436b791b7865ab1c3056de1eac
Lokesh <86400545+lokesh-clari@users.noreply.github.com>
2022-07-17 17:59:14 +0530

    Initializing SpringBoot Project

---
+++ AFProductionMonitor/src/main/java/com/example/AFProductionMonitor/AfProductionMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.AFProductionMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfProductionMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfProductionMonitorApplication.class, args);
+	}
+
+}
---
+++ AFProductionMonitor/src/test/java/com/example/AFProductionMonitor/AfProductionMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AFProductionMonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfProductionMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
